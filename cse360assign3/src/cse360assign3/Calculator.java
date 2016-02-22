4be508d968d2f84088edbb0dd686e9d3555cd285
priyanka7610 <priyanka.pakhale54.gmail.com>
2016-02-22 12:37:05 -0700

    Initial checkin of CSE360 Assignment 3.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,66 @@
+package cse360assign3;
+
+/** 
+ * 
+ * @author Priyanka
+ * @version Feb 18, 2016
+ *
+ */
+
+/** Class calculator performs 4 types of operations addition, 
+ * subtraction, multiplication and division and also returns the history
+ */
+public class Calculator {
+
+	private int total;
+	
+	/** Constructor of class Calculator **/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/** 
+	 * @return sum **/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Addition operation
+	 * @param operand **/
+	public void add (int value) {
+		
+	}
+	
+	/** Subtraction operation
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiplication operation
+	 * 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Division operation
+	 * 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** To get the history
+	 * 
+	 * @return history
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Priyanka
+ *
+ */
+public class CalculatorTest {
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#Calculator()}.
+	 */
+	@Test
+	public void testCalculator() {
+		Calculator c = new Calculator();
+		assertTrue(c != null);
+		
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#getTotal()}.
+	 */
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#add(int)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#subtract(int)}.
+	 */
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#multiply(int)}.
+	 */
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#divide(int)}.
+	 */
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#getHistory()}.
+	 */
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
